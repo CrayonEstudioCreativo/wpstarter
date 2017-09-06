fd39a9f27826345bbd3ff6dadfbb87adeb19d6dc
Isaac Torres Michel <isaac@crayon.com.mx>
2017-09-05 23:44:27 -0500

    Adds boilerplate.

---
+++ config/salts.php
@@ -0,0 +1,9 @@
+<?php
+define('AUTH_KEY',         'h{<=2X1GZ.KzW7J|E&[ZmmYVY<iBM]-gIUU8k|O[l?HCVb}S(/UVa-w*vo%XewnX');
+define('SECURE_AUTH_KEY',  'oOcL$|ng9XUmbKX&cG{-Is~Qmpvv6,T>*vCsi?xveegX:cAN8sWX:;Wo~_98l#FE');
+define('LOGGED_IN_KEY',    '+^JKumv%}b-U]D~IC-1{VR^?lt}K(T4.,Q%:R8Aqx|dAh}ATG&HlXSKjL#Q0wFD1');
+define('NONCE_KEY',        '/g)_yo6E22JHd93RXb16nA< 5$CbLV(^Cpz7:_Iu^nn 47w1#b3`1(MqJnM;Y+oC');
+define('AUTH_SALT',        '/*]1emz&8j~2~+LBOV0x;(#d--#obGQBI3gpSQ9zt+eD+9!A~BL|@mh&B8TixTcx');
+define('SECURE_AUTH_SALT', '.vMIg|k9?n9Hq9R^D:*N/:Ivze87{ >V<Bb)#;#L=ms?*.)x9E706bS5b!u]Gb?/');
+define('LOGGED_IN_SALT',   '|NnvVNjBD)}PTrK|N]3wD5aovRN=Ao# 5hx)u4l&,alG5/epRMKC]iS-:VXGJ$j8');
+define('NONCE_SALT',       '+AJ2u67!Yz]$*5k}YF90fL4Ry=B.*ow*1O?|7cB4H1biu[=_nwqB+:8z|fYsv2NK');
---
+++ src/Utils/SaltsGenerator.php
@@ -0,0 +1,46 @@
+<?php
+namespace isaactorresmichel\WordPress\Utils;
+
+
+class SaltsGenerator
+{
+    private $options = [];
+
+    public function __construct($options = [])
+    {
+        $this->options += $options;
+    }
+
+    private function getFromApi()
+    {
+        if (PHP_SAPI !== 'cli') {
+            throw new \Exception('SaltsGenerator should be invoked via the CLI version of PHP, not the ' . PHP_SAPI . ' SAPI' . PHP_EOL);
+        }
+
+        $response = file_get_contents('https://api.wordpress.org/secret-key/1.1/salt/');
+
+        if (!$response) {
+            throw new \Exception("Couldn't generate salts keys from API.");
+        }
+
+        $file = getcwd() . '/config/salts.php';
+
+        if (file_exists($file)) {
+            unlink($file);
+        }
+
+        if (file_put_contents($file, "<?php\n" . $response)) {
+            echo "Salts generated.\n";
+            return;
+        }
+
+        throw new \Exception("Couldn't generate salts keys from API.");
+    }
+
+    public static function generate()
+    {
+        $instance = new static();
+        $instance->getFromApi();
+    }
+
+}
